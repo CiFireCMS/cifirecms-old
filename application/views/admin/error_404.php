b2d9ad452ca6ff6a6bc5a2ad3d01c941a57855dc
adimancifi <46333102+adimancifi@users.noreply.github.com>
2022-03-06 19:14:31 +0800

    v2.1.1

--- application/views/admin/error_404.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div class="page-inner">
-	<div class="text-center pd-t-50">
-		<h1 class="error-title">404</h1>
-		<h3>Oops, an error has occurred. Page not found!</h3>
-	</div>
-</div>
